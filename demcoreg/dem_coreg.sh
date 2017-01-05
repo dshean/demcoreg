da4b164a919f1e1710cc6d32a17476114d85aaf1
David Shean <dshean@gmail.com>
2017-01-04 19:56:49 -0800

    added dem_coreg and dem_coreg_all

---
+++ demcoreg/dem_coreg.sh
@@ -0,0 +1,66 @@
+#! /bin/bash 
+
+#David Shean
+#dshean@gmail.com
+
+#This will co-register a single DEM
+#See dem_coreg_all.sh for batch processing
+
+#Input should be highest res version of DEM (i.e., DEM_2m.tif)
+dem=$1
+
+if [ ! -e $dem ] ; then
+    echo "Unable to find source DEM: $dem"
+    exit
+fi
+
+#Define the reference DEM
+#Need to create vrt with 1 arcsec over areas where 1/3 is not avail
+#1-arcsec NED (30 m) for CONUS
+#ref=/nobackup/deshean/rpcdem/ned1/ned1_tiles_glac24k_115kmbuff.vrt
+#1/3-arcsec NED (10 m) for CONUS
+#ref=/nobackup/deshean/rpcdem/ned13/ned13_tiles_glac24k_115kmbuff.vrt
+#1-arcsec SRTM (30 m) for HMA
+ref=/nobackup/deshean/rpcdem/hma/srtm1/hma_srtm_gl1.vrt
+
+if [ ! -e $ref ] ; then
+    echo "Unable to find ref DEM: $ref"
+    exit
+fi
+
+demdir=$(dirname $dem)
+dembase=$(basename $dem)
+#This will be pc_align output directory
+outdir=${dembase%.*}_grid_align
+dembase=$(echo $dembase | awk -F'-' '{print $1}')
+
+#This is DEM_32m reference mask output by dem_mask.py
+dem_mask=$demdir/${dembase}-DEM_32m_ref.tif
+
+if [ ! -e $dem_mask ] ; then
+    echo "Unable to find reference DEM mask, need to run dem_mask.py"
+    exit
+fi
+
+#Clip the reference DEM to the DEM_32m extent
+warptool.py -te $dem -tr $ref -t_srs $dem -outdir $demdir $ref
+
+refdem=$demdir/$(basename $ref)
+refdem=${refdem%.*}_warp.tif
+#Mask the ref using valid pixels in DEM_32m_ref.tif product
+apply_mask.py -extent intersection $refdem $dem_mask
+refdem_masked=${refdem%.*}_masked.tif
+
+#point-to-point
+pc_align_wrapper.sh $refdem_masked $dem
+
+cd $demdir
+log=$(ls -t $outdir/*.log | head -1)
+if [ -e $log ] ; then 
+    if grep -q 'Translation vector' $log ; then
+        apply_dem_translation.py ${dembase}-DEM_32m.tif $log
+        apply_dem_translation.py ${dembase}-DEM_8m.tif $log
+        ln -s $outdir/*DEM.tif ${dembase}-DEM_2m_trans.tif
+        compute_dh.py $(basename $refdem) ${dembase}-DEM_8m_trans.tif
+    fi
+fi
---
+++ demcoreg/dem_coreg_all.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+#David Shean
+#dshean@gmail.com
+
+#This will co-register a large batch of DEMs
+
+#Run tpfe1 or bro node
+#qsub ~/bin/devel.pbs
+#cd topdir
+
+#Default is for all WV/GE/QB subdir
+#list=$(ls -Sr *00/dem*/*-DEM_32m.tif | awk -F'/' '{print $1}')
+list_32m=$(ls -Sr *00/dem*/*-DEM_32m.tif | head -16)
+list_dir=$(ls -Sr *00/dem*/*-DEM_32m.tif | awk -F'/' '{print $1}' | head -16)
+
+#If we have existing orthoimages, compute top-of-atmosphere reflectance 
+#Uses toa.sh, toa.py, and dglib from https://github.com/dshean/dgtools 
+parallel --jobs 16 --delay 3 'toa.sh {}' ::: $list_dir
+
+#Now create masks for each 32m DEM
+#Check settings for dem_mask - MODSCAG, SNODAS, TOA, etc.
+parallel --jobs 16 --delay 3 'dem_mask.py {}' ::: $list_32m
+
+#Clean up existing pc_align runs
+#rm -r */*/*align */*/*trans.tif */*/*trans_dz_eul.tif 
+
+#Do the co-registration
+#set pc_align_wrapper threads to 2 or 4
+#set pc_align-wrapper max displacement
+list_2m=$(echo $list_32m | sed 's/-DEM_32m.tif/-DEM_2m.tif/g')
+parallel -j 16 --delay 3 'dem_coreg.sh {}' ::: $list_2m
+
+#Now create new weighted average mosaics, burn into reference DEM, and rerun the co-registration
