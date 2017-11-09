5ab50b5f3bf4f4efb347bc69311292687475bc03
David Shean <dshean@gmail.com>
2017-11-09 12:09:27 -0800

    get_natearth: added

---
+++ demcoreg/get_natearth.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+if [ -z "$DATADIR" ] ; then
+    export DATADIR=$HOME/data
+fi
+
+echo "Downlaiding Natural Earth layers"
+cd $DATADIR
+mkdir NaturalEarth
+cd NaturalEarth
+
+pwd
+
+wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/10m_cultural.zip
+unzip -d 10m_cultural 10m_cultural.zip
+
+wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/50m/cultural/50m_cultural.zip
+unzip -d 50m_cultural 50m_cultural.zip
+
+wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/physical/10m_physical.zip
+unzip -d 10m_physical 10m_physical.zip
