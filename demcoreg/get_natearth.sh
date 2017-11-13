fa6b63364e3fac81038cbdb2be2a9f14d0f5f181
David Shean <dshean@gmail.com>
2017-11-12 16:39:02 -0800

    get_natearth: update

--- demcoreg/get_natearth.sh
+++ demcoreg/get_natearth.sh
@@ -11,11 +11,14 @@ cd NaturalEarth
 
 pwd
 
-wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/10m_cultural.zip
-unzip -d 10m_cultural 10m_cultural.zip
-
+echo "50M Cultural"
 wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/50m/cultural/50m_cultural.zip
 unzip -d 50m_cultural 50m_cultural.zip
 
+echo "10M Cultural"
+wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/10m_cultural.zip
+unzip -d 10m_cultural 10m_cultural.zip
+
+echo "10M Physical"
 wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/physical/10m_physical.zip
 unzip -d 10m_physical 10m_physical.zip
