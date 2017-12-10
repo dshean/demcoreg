76142b994b4ab6020dc002324041bd460a3b9ed5
David Shean <dshean@gmail.com>
2017-12-10 15:34:35 -0800

    get_rgi: Create aea shp for regions

--- demcoreg/get_rgi.sh
+++ demcoreg/get_rgi.sh
@@ -78,16 +78,20 @@ ogr_merge.sh rgi60_merge.shp *_rgi60_*shp
 
 site=HMA
 wkt='POLYGON ((66 47, 106 47, 106 25, 66 25, 66 47))'
+aea_proj='+proj=aea +lat_1=25 +lat_2=47 +lat_0=36 +lon_0=85 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs '
 if [ ! -e rgi60_merge_${site}.shp ] ; then 
     echo "Clipping to site: $site"
     ogr2ogr -progress -overwrite -clipsrc "$wkt" rgi60_merge_${site}.shp rgi60_merge.shp
+    ogr2ogr -t_srs "$aea_proj" rgi60_merge_${site}_aea.shp rgi60_merge_${site}.shp
 fi
 
 site=CONUS
 wkt='POLYGON ((-125 49, -104 49, -104 32, -125 32, -125 49))'
+aea_proj='+proj=aea +lat_1=36 +lat_2=49 +lat_0=43 +lon_0=-115 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs '
 if [ ! -e rgi60_merge_${site}.shp ] ; then 
     echo "Clipping to site: $site"
     ogr2ogr -progress -overwrite -clipsrc "$wkt" rgi60_merge_${site}.shp rgi60_merge.shp
+    ogr2ogr -t_srs "$aea_proj" rgi60_merge_${site}_aea.shp rgi60_merge_${site}.shp
 fi
 
 echo $rgi_fn/regions/rgi60_merge.shp
