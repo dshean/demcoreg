67e86d2a36684a478968405f1f1ecf94a3dcd1dc
David Shean <dshean@gmail.com>
2017-12-10 20:43:36 -0800

    dem_coreg_all: update, wip

--- demcoreg/dem_coreg_all.sh
+++ demcoreg/dem_coreg_all.sh
@@ -30,14 +30,16 @@ clean=false
 
 #Copy DEMs and toa to subdir
 #mkdir dem_coreg
-#rsync -av --include='*-DEM_*.tif' --include '*toa.tif' --include='*/' --exclude='*' *00 dem_coreg/
+#shiftc -r -d --include='-DEM_2m.tif' --include='-DEM_32m.tif' --include='-DEM_8m.tif' --include 'toa.tif' *00 dem_coreg/
+##rsync -aLmv --include='*-DEM_*.tif' --include '*toa.tif' --include='*/' --exclude='*' *00 dem_coreg/
+##rsync -aLmv --include='*-DEM_2m.tif' --include='*-DEM_32m.tif' --include='*-DEM_8m.tif' --include '*toa.tif' --include='*/' --exclude='*' *00 dem_coreg/
 #cd dem_coreg
+
+#NOTE: Careful with QB DEMs here, need to increase max offset
 
 #Default is for all WV/GE/QB subdir
 list_dir=$(ls -Sr *00/dem*/*-DEM_32m.tif | awk -F'/' '{print $1}')
 list_32m=$(ls -Sr *00/dem*/*-DEM_32m.tif)
-#list_2m=$(ls -Sr *00/dem*/*-DEM_2m.tif)
-list_2m=$(echo $list_32m | sed 's/-DEM_32m.tif/-DEM_2m.tif/g')
 list_32m_done=$(ls -Sr *00/dem*/*-DEM_32m_trans.tif)
 
 njobs=10
@@ -79,6 +81,8 @@ echo "$ntodo todo"
 echo
 
 list_32m=$(cat ${todo_fn})
+#list_2m=$(ls -Sr *00/dem*/*-DEM_2m.tif)
+list_2m=$(echo $list_32m | sed 's/-DEM_32m.tif/-DEM_2m.tif/g')
 
 #Clean up existing masks
 if $clean ; then 
@@ -90,8 +94,9 @@ fi
 #Check settings for dem_mask - MODSCAG, SNODAS, TOA, etc.
 #Note that parallel won't work with input for modscag username
 #parallel --delay 1 'dem_mask.py {}' ::: $list_32m
+#parallel --delay 1 'dem_mask.py --bareground_thresh 5 {}' ::: $list_32m
 #Use TOA mask
-parallel --delay 1 'dem_mask.py --toa {}' ::: $list_32m
+#parallel --delay 1 'dem_mask.py --toa {}' ::: $list_32m
 #SnowEx
 #parallel --jobs 32 --delay 1 'dem_mask.py --filter not_forest+not_water --toa {}' ::: $list_32m
 
@@ -100,31 +105,39 @@ if $clean ; then
     rm -rv *00/*/*align *00/*/*trans.tif 
 fi
 
-#Input reference DEM
-#Should be existing 2-m mosaic for entire region
-#ref=/nobackup/deshean/rpcdem/lidar/conus_lidar_1m.vrt
-ref=$1
-#If we don't find it, create a reference
-if [ ! -n "$ref" ] ; then 
-    #For larger extents, need to run dem_mosaic_validtiles.py
-    dem_mosaic -o ref_DEM_2m_mos $list_2m
-    ref=ref_DEM_2m_mos-tile-0.tif
-    #Coreg round number
-    n=1
-fi
-
 #Do the co-registration
 #set pc_align_wrapper threads to 2 or 4
 #set pc_align-wrapper max displacement
 
-#Using gridded DEM as reference
-parallel --progress -j $njobs --delay 3 "dem_coreg.sh {} $ref" ::: $list_2m
+#Reference is a csv of xyz points
+if True ; then
+    #For output of filter_glas.py: ICESat-1 GLAS points over reference surfaces
+    list_csv=$(echo $list_2m | sed 's/DEM_2m.tif/DEM_32m_GLAH14_conus_refdemfilt_ref.csv/g')
+    parallel --link --progress -j $njobs --delay 3 "dem_coreg.sh {1} {2}" ::: $list_2m ::: $list_csv
+    #This should work for direct parallel command substitution, but there is still some syntax issue
+    ##parallel --plus --progress -j $njobs --delay 3 "dem_coreg.sh {} {/DEM_2m.tif/DEM_32m_GLAH14_conus_refdemfilt_ref.csv}" ::: $list_2m
+else
+    #Reference is a gridded DEM
+    #Should be existing 2-m mosaic for entire region
+    #ref=/nobackup/deshean/rpcdem/lidar/conus_lidar_1m.vrt
+    ref=$1
+    #If we don't find it, create a reference
+    if [ ! -n "$ref" ] ; then 
+        ref=ref_DEM_2m_mos-tile-0.tif
+        if [ ! -e $ref ] ; then 
+            #For larger extents, need to run dem_mosaic_validtiles.py
+            #Create mosaic, removing any low-quality products from QB02 or IK01
+            #dem_mosaic -o ref_DEM_2m_mos $(echo $list_2m | tr ' ' '\n' | grep -v QB02 | grep -v IK01)
+            dem_mosaic -o ref_DEM_2m_mos $(echo $list_2m | tr ' ' '\n' | grep -v '_101*_101')
+            ref=ref_DEM_2m_mos-tile-0.tif
+            #Coreg round number
+            n=1
+        fi
+    fi
 
-#Using csv of points as reference
-#list_csv=$(echo $list_2m | sed 's/DEM_2m.tif/DEM_32m_GLAH14_conus_refdemfilt_ref.csv/g')
-#parallel --link --progress -j $njobs --delay 3 "dem_coreg.sh {1} {2}" ::: $list_2m ::: $list_csv
-#This should work for parallel command substitution, but not
-##parallel --plus --progress -j $njobs --delay 3 "dem_coreg.sh {} {/DEM_2m.tif/DEM_32m_GLAH14_conus_refdemfilt_ref.csv}" ::: $list_2m
+    #Using gridded DEM as reference
+    parallel --progress -j $njobs --delay 3 "dem_coreg.sh {} $ref" ::: $list_2m
+fi
 
 #Now create new weighted average mosaics, burn into reference DEM, and rerun the co-registration
 #Should use make_mos.sh - need to specify that we want to use -DEM_2m_trans.tif
