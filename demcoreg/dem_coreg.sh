31e1fce837a06d0db512cf7c7eafde16a499f6f7
David Shean <dshean@gmail.com>
2018-07-12 09:46:10 -0700

    dem_coreg: clean up, use robust_stats instead of stats

--- demcoreg/dem_coreg.sh
+++ demcoreg/dem_coreg.sh
@@ -6,6 +6,11 @@
 #Exit if error
 set -e
 
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $(basename $0) source_dem ref_dem"
+    exit 1
+fi
+
 #Input should be highest res version of DEM (i.e., DEM_2m.tif)
 dem=$1
 if [ ! -e $dem ] ; then
@@ -36,38 +41,6 @@ if [ "${ref##*.}" == "csv" ] ; then
     #This will be pc_align output directory
     outdir=${dembase%.*}_pt_align
 else
-    #Hardcode reference DEM
-    #1-arcsec SRTM (30 m) for HMA
-    #ref=/nobackup/deshean/rpcdem/hma/srtm1/hma_srtm_gl1.vrt
-    #Round 1 after ICESat
-    #ref=/nobackupp8/deshean/hma/hma1_2016dec22/hma_2m_tile_20170220/hma_2m.vrt
-    #CONUS
-    #Need to create vrt with 1 arcsec over areas where 1/3 is not avail
-    #1-arcsec NED (30 m) for CONUS
-    #ref=/nobackup/deshean/rpcdem/ned1/ned1_tiles_glac24k_115kmbuff.vrt
-    #1/3-arcsec NED (10 m) for CONUS
-    #ref=/nobackup/deshean/rpcdem/ned13/ned13_tiles_glac24k_115kmbuff.vrt
-    #1-m lidar vrt
-    #ref=/nobackup/deshean/rpcdem/lidar/conus_lidar_1m.vrt
-    #2-m WV DEM mosaic, first round
-    #ref=/nobackup/deshean/conus/dem2/conus_coreg1_mos_2m_tile/conus_coreg1_mos_2m.vrt
-    #2-m WV DEM mosaic, second round
-    #ref=/nobackup/deshean/conus/dem2/conus_coreg2_mos_2m_tile/conus_coreg2_mos_2m.vrt
-    #2-m WV DEM mosaic, second round
-    #ref=/nobackup/deshean/conus/dem2/conus_coreg3_mos_2m_tile/conus_coreg3_mos_2m.vrt
-    #ref=/nobackup/deshean/conus/dem2/conus_coreg3_mos_2m_summer_tile/conus_coreg3_mos_2m_summer.vrt
-    #Ngozumpa
-    #2-m WV DEM mosaic
-    #ref=/nobackupp8/deshean/hma/ngozumpa2/hma1_2016dec22/stereo/ngozumpa2_2m_ref-tile-0.tif
-    #Mashel
-    #ref=/nobackupp8/deshean/mashel/Rasters/dem_3ft_unitmeters-adj.tif
-    #SnowEx
-    #SBB
-    #ref=/nobackup/deshean/snowex/aso/USCOSB20160926f1a1_dsm_vf_bf_masked.tif
-    #GM
-    #ref=/nobackup/deshean/snowex/aso/USCOGM20160926f1a1_dsm_1p5m_vf_bf_masked_trim.tif
-    #ref=/nobackup/deshean/snowex/stereo/gm/20160925_gm_2m_trans-tile-0.tif
-
     #This will be pc_align output directory
     outdir=${dembase%.*}_grid_align
 
@@ -91,10 +64,8 @@ else
         echo
         echo "Warping reference DEM to low-res mask extent"
         warptool.py -r near -te $dem_mask -tr $dem_mask -t_srs $dem_mask -outdir $demdir $ref
-        stats=$(~/src/demtools/stats.py $refdem)
-        echo
-        echo $stats
-        count=$(echo $stats | awk '{print $2}')
+        #This does a bunch of unnecessary calculations, could do quick python call to get valid px count
+        count=$(robust_stats.py $refdem | grep 'Count:' | awk '{print $2}')
         mincount=100
         if [ "$count" -lt "$mincount" ] ; then
             echo "Not enough valid pixels in clipped reference DEM: (${count} < ${mincount})" 
@@ -103,10 +74,7 @@ else
         echo
         echo "Applying low-res mask to high-res reference DEM"
         apply_mask.py -extent intersection $refdem $dem_mask
-        stats=$(~/src/demtools/stats.py $refdem_masked)
-        echo
-        echo $stats
-        count=$(echo $stats | awk '{print $2}')
+        count=$(robust_stats.py $refdem_masked | grep 'Count:' | awk '{print $2}')
         mincount=100
         if [ "$count" -lt "$mincount" ] ; then
             echo "Not enough valid pixels in masked reference DEM: (${count} < ${mincount})" 
