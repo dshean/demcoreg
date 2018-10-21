283a891b8c3e56a158df8b3d20c55ac46ac34e04
David Shean <dshean@gmail.com>
2018-10-21 13:59:15 -0700

    dem_align: clean up pull request, better ds handling, bugfixes
    Close datasets when no longer needed
    Fix static_mask changes
    Fix geolib.gdaldem_mem_ma issue responsible ds update error
    Handle case where fit is None, see changes in coreglib
    Clean up filename variables
    Add colorbar to ref and source plots in output png

--- demcoreg/dem_align.py
+++ demcoreg/dem_align.py
@@ -45,7 +45,7 @@ def getparser():
     parser.add_argument('-outdir', default=None, help='Output directory')
     return parser
 
-def get_mask(ds, dem_fn, filter='not_forest', mask_glaciers=True, bareground_thresh=60):
+def get_mask(ds, dem_fn=None, filter='not_forest', mask_glaciers=True, bareground_thresh=60):
     #This logic needs to be cleaned up
     if filter != 'none':
         #Mask glaciers, vegetated slopes
@@ -53,13 +53,14 @@ def get_mask(ds, dem_fn, filter='not_forest', mask_glaciers=True, bareground_thr
     else:
         #Mask glaciers only
         static_mask = dem_mask.get_icemask(ds)
-    #Top-of-atmosphere reflectance threshold (requires orthoimage and output from toa.sh)
-    toa_fn = dem_mask.get_toa_fn(dem_fn)
-    #toa_fn = None
-    if toa_fn is not None:
-        toa_ds = warplib.memwarp_multi_fn([toa_fn,], res=ds, extent=ds, t_srs=ds, r='cubicspline')[0]
-        toa_mask = dem_mask.get_toa_mask(toa_ds)
-        static_mask = np.logical_and(static_mask, toa_mask)
+    if dem_fn is not None:
+        #Top-of-atmosphere reflectance threshold (requires orthoimage and output from toa.sh)
+        toa_fn = dem_mask.get_toa_fn(dem_fn)
+        #toa_fn = None
+        if toa_fn is not None:
+            toa_ds = warplib.memwarp_multi_fn([toa_fn,], res=ds, extent=ds, t_srs=ds, r='cubicspline')[0]
+            toa_mask = dem_mask.get_toa_mask(toa_ds)
+            static_mask = np.logical_and(static_mask, toa_mask)
     #Return final mask, ready to be applied
     return ~(static_mask)
 
@@ -97,6 +98,8 @@ def compute_offset(dem1_ds, dem2_ds, dem2_fn, mode='nuth', max_offset_m=100, rem
         diff_euler = np.ma.array(diff_euler, mask=static_mask)
         static_mask = np.ma.getmaskarray(diff_euler)
 
+    dem2_clip_ds = None
+
     if diff_euler.count() == 0:
         sys.exit("No overlapping, unmasked pixels shared between input DEMs")
 
@@ -122,6 +125,10 @@ def compute_offset(dem1_ds, dem2_ds, dem2_fn, mode='nuth', max_offset_m=100, rem
     #By default, don't create output figure
     fig = None
 
+    #Default horizntal shift is (0,0)
+    dx = 0
+    dy = 0
+
     #Sum of absolute differences
     if mode == "sad":
         m, int_offset, sp_offset = coreglib.compute_offset_sad(dem1, dem2, pad=pad)
@@ -141,16 +148,20 @@ def compute_offset(dem1_ds, dem2_ds, dem2_fn, mode='nuth', max_offset_m=100, rem
         print("Computing slope and aspect")
         dem1_slope = geolib.gdaldem_mem_ds(dem1_clip_ds, processing='slope', returnma=True)
         dem1_aspect = geolib.gdaldem_mem_ds(dem1_clip_ds, processing='aspect', returnma=True)
+        dem1_clip_ds = None
         #Compute relationship between elevation difference, slope and aspect
         fit_param, fig = coreglib.compute_offset_nuth(diff_euler, dem1_slope, dem1_aspect, plot=False)
-        #fit_param[0] is magnitude of shift vector
-        #fit_param[1] is direction of shift vector
-        #fit_param[2] is mean bias divided by tangent of mean slope
-        #print(fit_param)
-        dx = fit_param[0]*np.sin(np.deg2rad(fit_param[1]))
-        dy = fit_param[0]*np.cos(np.deg2rad(fit_param[1]))
-        #med_slope = malib.fast_median(dem1_slope)
-        #dz = fit_param[2]*np.tan(np.deg2rad(med_slope))
+        if fit_param is None:
+            print("Failed to calculate horizontal shift")
+        else:
+            #fit_param[0] is magnitude of shift vector
+            #fit_param[1] is direction of shift vector
+            #fit_param[2] is mean bias divided by tangent of mean slope
+            #print(fit_param)
+            dx = fit_param[0]*np.sin(np.deg2rad(fit_param[1]))
+            dy = fit_param[0]*np.cos(np.deg2rad(fit_param[1]))
+            #med_slope = malib.fast_median(dem1_slope)
+            #dz = fit_param[2]*np.tan(np.deg2rad(med_slope))
     elif mode == "all":
         print("Not yet implemented")
         #Want to compare all methods, average offsets
@@ -202,19 +213,21 @@ def main2(args):
     print("Valid Surface Type Filter: %s" % filter)
     print("Output: %s\n" % outprefix)
 
-    dem2_ds = gdal.Open(dem2_fn, gdal.GA_Update)
+    #dem2_ds = gdal.Open(dem2_fn, gdal.GA_Update)
     #open source dataset in update mode, avoids File not open for writing and
     #avoids program to crash in case of a very big dataset, after running for
     #several iterations as mentioned in the link
     #https://github.com/mapbox/rasterio/issues/307
-    #Often the "ref" DEM is high-res lidar or similar
-    #This is a shortcut to resample to match "source" DEM
-    dem1_ds = warplib.memwarp_multi_fn([dem1_fn,], res=dem2_ds, extent=dem2_ds, t_srs=dem2_ds, r='cubic')[0]
-    #dem1_ds = gdal.Open(dem1_fn, gdal.GA_ReadOnly)
-
+    dem2_ds = gdal.Open(dem2_fn)
     #Create a copy to be updated in place
     dem2_ds_align = iolib.mem_drv.CreateCopy('', dem2_ds, 0)
-    #dem2_ds_align = dem2_ds
+    #This is another approach that duplicates CreateCopy functionality
+    #dem2_ds_align = geolib.mem_ds_copy(dem2_ds)
+    dem2_ds = None
+
+    #Often the "ref" DEM is high-res lidar or similar
+    #This is a shortcut to resample to match "source" DEM
+    dem1_ds = warplib.memwarp_multi_fn([dem1_fn,], res=dem2_ds_align, extent=dem2_ds_align, t_srs=dem2_ds_align, r='cubic')[0]
 
     #Iteration number
     n = 1
@@ -277,10 +290,17 @@ def main2(args):
 
     #Compute original elevation difference
     if True:
+        dem2_ds = gdal.Open(dem2_fn)
         dem1_clip_ds, dem2_clip_ds = warplib.memwarp_multi([dem1_ds, dem2_ds], \
                 res='max', extent='intersection', t_srs=dem2_ds, r='cubic')
+        dem2_ds = None
         dem1_orig = iolib.ds_getma(dem1_clip_ds, 1)
         dem2_orig = iolib.ds_getma(dem2_clip_ds, 1)
+        #Needed for plotting
+        dem1_hs = geolib.gdaldem_mem_ds(dem1_clip_ds, processing='hillshade', returnma=True)
+        dem2_hs = geolib.gdaldem_mem_ds(dem2_clip_ds, processing='hillshade', returnma=True)
+        dem2_clip_ds = None
+        res = float(geolib.get_res(dem1_clip_ds, square=True)[0])
         diff_euler_orig = dem2_orig - dem1_orig
         if not apply_mask:
             static_mask_orig = np.ma.getmaskarray(diff_euler_orig)
@@ -289,8 +309,9 @@ def main2(args):
 
         #Write out original eulerian difference map
         print("Writing out original euler difference map for common intersection before alignment")
-        dst_fn = outprefix + '_orig_dz_eul.tif'
-        iolib.writeGTiff(diff_euler_orig, dst_fn, dem1_clip_ds)
+        orig_eul_fn = outprefix + '_orig_dz_eul.tif'
+        iolib.writeGTiff(diff_euler_orig, orig_eul_fn, dem1_clip_ds)
+        dem1_clip_ds = None
 
     #Compute final elevation difference
     if True:
@@ -298,14 +319,18 @@ def main2(args):
                 res='max', extent='intersection', t_srs=dem2_ds_align, r='cubic')
         dem1_align = iolib.ds_getma(dem1_clip_ds_align, 1)
         dem2_align = iolib.ds_getma(dem2_clip_ds_align, 1)
+        #Need this for scalebar plot
+        #res = float(geolib.get_res(dem2_clip_ds_align, square=True)[0])
         diff_euler_align = dem2_align - dem1_align
+        dem2_align = None
+        dem1_align = None
         if not apply_mask:
             static_mask = np.ma.getmaskarray(diff_euler_align)
-        #This can fail due to shifts - need to recompute
-        #IndexError: boolean index did not match indexed array along dimension 0; dimension is 2244 but corresponding boolean dimension is 2246
-        #added this for above mentioned boolean index error
-        static_mask_final = get_mask(dem2_clip_ds_align,dem2_fn, filter=filter)
-        diff_euler_align_compressed = diff_euler_align[~static_mask_final]
+        #Get updated, final mask
+        static_mask = get_mask(dem2_clip_ds_align, dem2_fn, filter=filter)
+        dem2_clip_ds_align = None
+        static_mask = np.ma.getmaskarray(np.ma.array(diff_euler_align, mask=static_mask))
+        diff_euler_align_compressed = diff_euler_align[~static_mask]
         diff_euler_align_stats = np.array(malib.print_stats(diff_euler_align_compressed))
 
         #Fit plane to residuals and remove
@@ -316,7 +341,7 @@ def main2(args):
             #Note that the origmask=False will compute vals for all x and y indices, which is what we want
             #Should offer option for polynomial of arbitrary order
             #Also, want a better robust fit - maybe throw out more outliers
-            vals, resid, coeff = geolib.ma_fitplane(np.ma.array(diff_euler_align, mask=static_mask_final), \
+            vals, resid, coeff = geolib.ma_fitplane(np.ma.array(diff_euler_align, mask=static_mask), \
                     gt, perc=(12.5, 87.5), origmask=False)
             #Remove planar offset from difference map
             diff_euler_align -= vals
@@ -328,55 +353,55 @@ def main2(args):
             dem2_ds_align = coreglib.apply_z_shift(dem2_ds_align, -vals, createcopy=False)
             if not apply_mask:
                 static_mask = np.ma.getmaskarray(diff_euler_align)
-            diff_euler_align_compressed = diff_euler_align[~static_mask_final]
+            diff_euler_align_compressed = diff_euler_align[~static_mask]
             diff_euler_align_stats = np.array(malib.print_stats(diff_euler_align_compressed))
             print("Creating fitplane plot")
             fig, ax = plt.subplots(figsize=(6, 6))
             fitplane_clim = malib.calcperc(vals, (2,98))
             im = ax.imshow(vals, cmap='cpt_rainbow', clim=fitplane_clim)
-            res = float(geolib.get_res(dem2_clip_ds, square=True)[0])
             pltlib.add_scalebar(ax, res=res)
             pltlib.hide_ticks(ax)
             pltlib.add_cbar(ax, im, label='Fit plane residuals (m)')
             fig.tight_layout()
-            dst_fn1 = outprefix + '%s_align_dz_eul_fitplane.png' % xyz_shift_str_cum
-            print("Writing out figure: %s" % dst_fn1)
-            fig.savefig(dst_fn1, dpi=300, bbox_inches='tight', pad_inches=0.1)
+            tiltcorr_fig_fn = outprefix + '%s_align_dz_eul_fitplane.png' % xyz_shift_str_cum
+            print("Writing out figure: %s" % tiltcorr_fig_fn)
+            fig.savefig(tiltcorr_fig_fn, dpi=300, bbox_inches='tight', pad_inches=0.1)
 
         #Compute higher-order fits?
         #Could also attempt to model along-track and cross-track artifacts
 
         #Write out aligned eulerian difference map for clipped extent with vertial offset removed
-        dst_fn = outprefix + '%s_align_dz_eul.tif' % xyz_shift_str_cum
+        align_eul_fn = outprefix + '%s_align_dz_eul.tif' % xyz_shift_str_cum
         print("Writing out aligned difference map with median vertical offset removed")
-        iolib.writeGTiff(diff_euler_align, dst_fn, dem1_clip_ds)
+        iolib.writeGTiff(diff_euler_align, align_eul_fn, dem1_clip_ds_align)
+        dem1_clip_ds_align = None
 
     #Write out aligned dem_2 with vertial offset removed
     if True:
-        dst_fn2 = outprefix + '%s_align.tif' % xyz_shift_str_cum
-        print("Writing out shifted dem2 with median vertical offset removed: %s" % dst_fn2)
+        align_fn = outprefix + '%s_align.tif' % xyz_shift_str_cum
+        print("Writing out shifted dem2 with median vertical offset removed: %s" % align_fn)
         #Might be cleaner way to write out MEM ds directly to disk
         dem2_align = iolib.ds_getma(dem2_ds_align)
-        iolib.writeGTiff(dem2_align, dst_fn2, dem2_ds_align)
+        iolib.writeGTiff(dem2_align, align_fn, dem2_ds_align)
+        dem2_align = None
         dem2_ds_align = None
 
     #Create output plot
     if True:
         print("Creating final plot")
-        dem1_hs = geolib.gdaldem_mem_ma(dem1_orig, dem1_clip_ds, returnma=True)
-        dem2_hs = geolib.gdaldem_mem_ma(dem2_orig, dem2_clip_ds, returnma=True)
         f, axa = plt.subplots(2, 3, figsize=(11, 8.5))
         for ax in axa.ravel()[:-1]:
             ax.set_facecolor('k')
             pltlib.hide_ticks(ax)
         dem_clim = malib.calcperc(dem1_orig, (2,98))
         axa[0,0].imshow(dem1_hs, cmap='gray')
-        axa[0,0].imshow(dem1_orig, cmap='cpt_rainbow', clim=dem_clim, alpha=0.6)
-        res = float(geolib.get_res(dem1_clip_ds, square=True)[0])
+        im = axa[0,0].imshow(dem1_orig, cmap='cpt_rainbow', clim=dem_clim, alpha=0.6)
+        pltlib.add_cbar(axa[0,0], im, label=None)
         pltlib.add_scalebar(axa[0,0], res=res)
         axa[0,0].set_title('Reference DEM')
         axa[0,1].imshow(dem2_hs, cmap='gray')
-        axa[0,1].imshow(dem2_orig, cmap='cpt_rainbow', clim=dem_clim, alpha=0.6)
+        im = axa[0,1].imshow(dem2_orig, cmap='cpt_rainbow', clim=dem_clim, alpha=0.6)
+        pltlib.add_cbar(axa[0,1], im, label=None)
         axa[0,1].set_title('Source DEM')
         axa[0,2].imshow(~static_mask_orig, clim=(0,1), cmap='gray')
         axa[0,2].set_title('Surfaces for co-registration')
@@ -412,21 +437,22 @@ def main2(args):
         f.tight_layout()
         plt.subplots_adjust(top=0.90)
 
-        dst_fn = outprefix + '%s_align.png' % xyz_shift_str_cum
-        print("Writing out figure: %s" % dst_fn)
-        f.savefig(dst_fn, dpi=300, bbox_inches='tight', pad_inches=0.1)
-        #Removing residual planar tilt can introduce additional slope/aspect dependent offset
-        #Want to run another round of main dem_align after removing planar tilt
-        if tiltcorr:
-            print("\n Rerunning after applying tilt correction \n")
-            #Create copy of original arguments
-            import copy
-            args2 = copy.copy(args)
-            #Use aligned, tilt-corrected DEM as input src_fn for second round
-            args2.src_fn = dst_fn2
-            #Assume we've already corrected most of the tilt during first round (also prevents endless loop)
-            args2.tiltcorr = False
-            main2(args2)
+        fig_fn = outprefix + '%s_align.png' % xyz_shift_str_cum
+        print("Writing out figure: %s" % fig_fn)
+        f.savefig(fig_fn, dpi=300, bbox_inches='tight', pad_inches=0.1)
+
+    #Removing residual planar tilt can introduce additional slope/aspect dependent offset
+    #Want to run another round of main dem_align after removing planar tilt
+    if tiltcorr:
+        print("\n Rerunning after applying tilt correction \n")
+        #Create copy of original arguments
+        import copy
+        args2 = copy.copy(args)
+        #Use aligned, tilt-corrected DEM as input src_fn for second round
+        args2.src_fn = align_fn 
+        #Assume we've already corrected most of the tilt during first round (also prevents endless loop)
+        args2.tiltcorr = False
+        main2(args2)
 
 def main(argv=None):
     parser = getparser()
