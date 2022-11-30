d0095ad41aacaa896d47fe5da22e329629d5ff5f
David Shean <dshean@gmail.com>
2022-11-29 20:07:16 -0800

    get_refdem: update with datum and reprojection, incremental improvement

--- demcoreg/get_refdem.sh
+++ demcoreg/get_refdem.sh
@@ -3,17 +3,34 @@
 #Uses OpenTopography API to fetch reference DEM
 #https://portal.opentopography.org/apidocs/#/Public/getGlobalDem
 
-#["SRTMGL3", "SRTMGL1", "SRTMGL1_E", "AW3D30", "AW3D30_E"]
-demtype="SRTMGL1_E"
+if [ "$#" -ne 7 ]; then
+    echo "Usage: $(basename $0) minlon minlat maxlon maxlat out_fn demtype epsg_code"
+    echo "Example for Copernicus 30 m DEM over Utqiagvik Alaska and UTM Zone 4N"
+    echo "$(basename $0) -157.5 71 -155.5 71.75 COP30_utqiagvik.tif COP30 32604"
+    exit 1
+fi
+
+#OT demo key (replace with user API key)
+API_key="demoapikeyot2022"
 
 minlon=$1
 minlat=$2
 maxlon=$3
 maxlat=$4
 
+#Output filename
 out_fn=$5
 
-url="https://portal.opentopography.org/API/globaldem?demtype=${demtype}&south=${minlat}&north=${maxlat}&west=${minlon}&east=${maxlon}&outputFormat=GTiff" 
+#['SRTMGL3', 'SRTMGL1', 'SRTMGL1_E', 'AW3D30', 'AW3D30_E', 'SRTM15Plus', 'NASADEM', 'COP30', 'COP90']
+#demtype="COP30"
+#Check against list
+demtype=$6
+
+#Example EPSG code for UTM Zone 12N (should compute automatically)
+#epsg=32612
+epsg=$7
+
+url="https://portal.opentopography.org/API/globaldem?API_Key=${API_key}&demtype=${demtype}&south=${minlat}&north=${maxlat}&west=${minlon}&east=${maxlon}&outputFormat=GTiff" 
 echo $url
 curl -X GET "$url" -H "accept: */*" --output $out_fn 
 
@@ -25,3 +42,9 @@ gdal_translate $opt $out_fn ${out_fn%.*}_lzw.tif
 #if [ $? -eq 0 ] ; then
 #    mv ${out_fn%.*}_lzw.tif $out_fn
 #fi
+
+#Convert to ellipsoid height (uses ASP dem_geoid utility, update to corresponding GDAL/PROJ command)
+dem_geoid --reverse-adjustment --geoid EGM2008 ${out_fn%.*}_lzw.tif
+
+#Reproject to UTM
+gdalwarp $gdal_opt -r cubic -t_srs EPSG:$epsg ${out_fn%.*}_lzw-adj.tif ${out_fn%.*}_lzw-adj_proj.tif
