4717175aa3913be540ff0d1e2565d92c6c41bbbb
David Shean <dshean@gmail.com>
2024-01-11 11:37:20 -0800

    Update setup for recent release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from distutils.core import setup
 #python setup.py sdist upload
 
 setup(name='demcoreg',
-    version='0.5.0',
+    version='1.1.2',
     description='Utilities for DEM co-registration',
     author='David Shean',
     author_email='dshean@gmail.com',
@@ -14,6 +14,7 @@ setup(name='demcoreg',
     url='https://github.com/dshean/demcoreg',
     packages=['demcoreg'],
     long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     install_requires=['numpy','gdal','pygeotools','wget'],
     #Note: this will create local copy of executable scripts
     #scripts=['demcoreg/pc_align_wrapper.sh', 'demcoreg/apply_dem_translation.py', 'demcoreg/compute_diff.py', 'demcoreg/dem_align.py', 'demcoreg/dem_mask.py', 'demcoreg/dem_coreg.sh', 'demcoreg/dem_coreg_all.sh', 'demcoreg/vol_stats.py', 'demcoreg/robust_stats.py', 'demcoreg/glas_proc.py', 'demcoreg/filter_glas.py', 'demcoreg/get_nlcd.sh', 'demcoreg/get_bareground.sh', 'demcoreg/get_rgi.sh']
